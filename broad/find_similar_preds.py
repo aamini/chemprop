e33f937e9788b594f6677f71552977a13f2a7c03
Kyle Swanson <swansonk.14@gmail.com>
2019-02-08 16:21:53 -0500

    Updating distance script

--- broad/find_similar_preds.py
+++ broad/find_similar_preds.py
@@ -5,6 +5,7 @@ sys.path.append('../')
 
 import numpy as np
 from scipy.spatial.distance import cdist
+from tqdm import tqdm
 
 from chemprop.data.utils import get_data
 from chemprop.features import morgan_fingerprint
@@ -16,23 +17,24 @@ from chemprop.utils import load_checkpoint
 def find_similar_preds(test_path: str,
                        train_path: str,
                        checkpoint_path: str,
+                       distance_measure: str,
                        save_path: str,
                        num_neighbors: int,
                        batch_size: int):
     print('Loading data')
     test_data, train_data = get_data(test_path), get_data(train_path)
     test_smiles, train_smiles = test_data.smiles(), train_data.smiles()
+    train_smiles_set = set(train_smiles)
 
     print('Computing morgan fingerprints')
-    test_morgans = np.array([morgan_fingerprint(smiles) for smiles in test_data.smiles()])
-    train_morgans = np.array([morgan_fingerprint(smiles) for smiles in train_data.smiles()])
+    test_morgans = np.array([morgan_fingerprint(smiles) for smiles in tqdm(test_data.smiles(), total=len(test_data))])
+    train_morgans = np.array([morgan_fingerprint(smiles) for smiles in tqdm(train_data.smiles(), total=len(train_data))])
 
     print('Loading model')
     model = load_checkpoint(checkpoint_path)
 
     print('Predicting')
     test_preds = predict(model=model, data=test_data, batch_size=batch_size)
-    # train_preds = predict(model=model, data=train_data, batch_size=batch_size)
 
     print('Computing molecule vectors')
     test_vecs = compute_molecule_vectors(model=model, data=test_data, batch_size=batch_size)
@@ -43,45 +45,57 @@ def find_similar_preds(test_path: str,
     test_train_morgan_dist = cdist(test_morgans, train_morgans, metric='jaccard')
     test_train_vec_dist = cdist(test_vecs, train_vecs, metric='cosine')
 
+    if distance_measure == 'vec':
+        dist_matrix = test_train_vec_dist
+    elif distance_measure == 'morgan':
+        dist_matrix = test_train_morgan_dist
+    else:
+        raise ValueError(f'Distance measure "{distance_measure}" not supported.')
+
     print('Finding neighbors')
     neighbors = []
     for test_index in range(len(test_data)):
-        # Find the num_neighbors molecules in the training set which are
-        # most similar to the test molecule by molecule vector distance
-        nearest_train_by_vec_dist = np.argsort(test_train_vec_dist[test_index])[:num_neighbors]
+        # Find the num_neighbors molecules in the training set which are most similar to the test molecule
+        nearest_train = np.argsort(dist_matrix[test_index])[:num_neighbors]
 
         # Get the distances from test molecule to the nearest train molecules
-        nearest_train_vec_dists = test_train_vec_dist[test_index][nearest_train_by_vec_dist]
-        nearest_train_morgan_dists = test_train_morgan_dist[test_index][nearest_train_by_vec_dist]
+        nearest_train_vec_dists = test_train_vec_dist[test_index][nearest_train]
+        nearest_train_morgan_dists = test_train_morgan_dist[test_index][nearest_train]
 
         # Build dictionary with distance info
         neighbor = {
             'test_smiles': test_smiles[test_index],
+            'in_train': test_smiles[test_index] in train_smiles_set,
             'test_pred': test_preds[test_index][0],
-            f'train_{num_neighbors}_avg_vec_dist': np.mean(nearest_train_vec_dists),
-            f'train_{num_neighbors}_avg_morgan_dist': np.mean(nearest_train_morgan_dists)
+            f'train_{num_neighbors}_avg_vec_cosine_dist': np.mean(nearest_train_vec_dists),
+            f'train_{num_neighbors}_avg_morgan_jaccard_dist': np.mean(nearest_train_morgan_dists)
         }
-        for i, train_index in enumerate(nearest_train_by_vec_dist):
+        for i, train_index in enumerate(nearest_train):
             neighbor[f'train_smiles_{i + 1}'] = train_smiles[train_index]
-            neighbor[f'train_vec_dist_{i + 1}'] = nearest_train_vec_dists[i]
-            neighbor[f'train_morgan_dist_{i + 1}'] = nearest_train_morgan_dists[i]
+            neighbor[f'train_vec_cosine_dist_{i + 1}'] = nearest_train_vec_dists[i]
+            neighbor[f'train_morgan_jaccard_dist_{i + 1}'] = nearest_train_morgan_dists[i]
 
         neighbors.append(neighbor)
+
+    # Sort by test prediction
     neighbors.sort(key=lambda neighbor: neighbor['test_pred'], reverse=True)
 
-    print('Saving distances')
+    # Construct fieldnames for save file
     fieldnames = [
         'test_smiles',
+        'in_train',
         'test_pred',
-        f'train_{num_neighbors}_avg_vec_dist',
-        f'train_{num_neighbors}_avg_morgan_dist'
+        f'train_{num_neighbors}_avg_vec_cosine_dist',
+        f'train_{num_neighbors}_avg_morgan_jaccard_dist'
     ]
     for i in range(num_neighbors):
         fieldnames += [
             f'train_smiles_{i + 1}',
-            f'train_vec_dist_{i + 1}',
-            f'train_morgan_dist_{i + 1}'
+            f'train_vec_cosine_dist_{i + 1}',
+            f'train_morgan_jaccard_dist_{i + 1}'
         ]
+
+    print('Saving distances')
     with open(save_path, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
@@ -99,6 +113,8 @@ if __name__ == '__main__':
                         help='Path to .pt file containing a model checkpoint')
     parser.add_argument('--save_path', type=str, required=True,
                         help='Path to CSV file where similar molecules will be saved')
+    parser.add_argument('--distance_measure', type=str, choices=['vec', 'morgan'], default='vec',
+                        help='Distance measure to use to find nearest neighbors in train set')
     parser.add_argument('--num_neighbors', type=int, default=5,
                         help='Number of neighbors to search for each molecule')
     parser.add_argument('--batch_size', type=int, default=50,
@@ -106,10 +122,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     find_similar_preds(
-        args.test_path,
-        args.train_path,
-        args.checkpoint_path,
-        args.save_path,
-        args.num_neighbors,
-        args.batch_size
+        test_path=args.test_path,
+        train_path=args.train_path,
+        checkpoint_path=args.checkpoint_path,
+        save_path=args.save_path,
+        distance_measure=args.distance_measure,
+        num_neighbors=args.num_neighbors,
+        batch_size=args.batch_size
     )
