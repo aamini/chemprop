59b41b9fd90abe73da45930f5d4e4b3a653fc861
Kyle Swanson <swansonk.14@gmail.com>
2019-04-12 16:15:17 -0400

    Updating chembl scraping script

--- broad/scrape_chembl_smiles.py
+++ broad/scrape_chembl_smiles.py
@@ -10,16 +10,18 @@ from tqdm import tqdm
 
 
 def get_smiles_from_id(id: str) -> str:
+    url = f'https://www.ebi.ac.uk/chembl/compound_report_card/{id}'
+
     try:
-        url = f'https://www.ebi.ac.uk/chembl/compound_report_card/{id}'
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         json_element = soup.find(id='JSON_LD')
         compound_info = json.loads(json_element.text)
         smiles = compound_info['canonical_smiles'][0]
     except Exception as e:
-        print(e)
-        print(id)
+        if "NoneType" in e.args[0]:
+            print('Please check the smiles for this compound manually')
+            print(url)
         return ''
 
     return smiles
@@ -32,7 +34,9 @@ def scrape_chembl_smiles(chembl_id_paths: List[str], chembl_smiles_path: str):
             lines = [line.replace('\0', '') for line in f]  # strip null characters
             ids += [row['ChEMBL ID'] for row in csv.DictReader(lines)]
 
-    print(f'Number of IDs = {len(ids)}')
+    ids = set(ids)
+
+    print(f'Number of unique IDs = {len(ids)}')
 
     smiles = []
     with Pool() as pool:
@@ -40,8 +44,11 @@ def scrape_chembl_smiles(chembl_id_paths: List[str], chembl_smiles_path: str):
             if smile != '':
                 smiles.append(smile)
 
-    print(f'Number of SMILES = {len(smiles)}')
     print(f'Number of invalid IDs = {len(ids) - len(smiles)}')
+
+    smiles = set(smiles)
+
+    print(f'Number of unique SMILES = {len(smiles)}')
 
     with open(chembl_smiles_path, 'w') as f:
         for smile in smiles:
@@ -51,7 +58,7 @@ def scrape_chembl_smiles(chembl_id_paths: List[str], chembl_smiles_path: str):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--chembl_id_paths', type=str, nargs='+', required=True,
-                        help='Path to .txt with ChEMBL IDs')
+                        help='Path to .csv with ChEMBL IDs (with "ChEMBL ID" in header row)')
     parser.add_argument('--chembl_smiles_path', type=str, required=True,
                         help='Path to a .txt where ChEMBL SMILES will be saved')
     args = parser.parse_args()
