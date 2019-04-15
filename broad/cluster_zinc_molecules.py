302041ed74cecf0c5198bcb8df90ec30c2fa04ef
Kyle Swanson <swansonk.14@gmail.com>
2019-04-14 20:43:23 -0400

    Updating clustering script

--- broad/cluster_zinc_molecules.py
+++ broad/cluster_zinc_molecules.py
@@ -7,43 +7,29 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import Draw
 from sklearn.cluster import KMeans
-from tqdm import tqdm
+from tqdm import tqdm, trange
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from chemprop.utils import makedirs
 from chemprop.features import get_features_generator
-from standardize_smiles import standardize_smiles
 
 
 def cluster_zinc_molecules(data_path: str,
                            save_dir: str,
-                           train_smiles_path: str,
-                           train_hits_smiles_path: str,
-                           num_clusters: int):
+                           num_clusters: int,
+                           sort_column: str):
     # Load data
     with open(data_path) as f:
         data = list(csv.DictReader(f))
-
-    with open(train_smiles_path) as f:
-        train_smiles = {row['smiles'] for row in csv.DictReader(f)}
-
-    with open(train_hits_smiles_path) as f:
-        train_hits_smiles = {row['smiles'] for row in csv.DictReader(f)}
-
-    # Standardize smiles
-    train_smiles_standardized = {standardize_smiles(smiles) for smiles in train_smiles}
-    train_hits_smiles_standardized = {standardize_smiles(smiles) for smiles in train_hits_smiles}
-
-    for row in data:
-        standardized = standardize_smiles(row['smiles'])
-        row['in_train'] = standardized in train_smiles_standardized
-        row['in_train_hit'] = standardized in train_hits_smiles_standardized
+    fieldnames = list(data[0].keys())
 
     # Compute Morgan fingerprints
     print('Computing Morgan fingerprints')
     morgan_generator = get_features_generator('morgan')
     morgans = [morgan_generator(datapoint['smiles']) for datapoint in tqdm(data, total=len(data))]
+    for datapoint, morgan in zip(data, morgans):
+        datapoint['morgan'] = morgan
 
     # Perform clustering
     print('Clustering')
@@ -54,47 +40,49 @@ def cluster_zinc_molecules(data_path: str,
     for cluster, datapoint in zip(kmeans.labels_, data):
         clusters[cluster].append(datapoint)
 
-    # Save clustering
-    print('Saving clusters and images')
-    max_cluster_size = max(len(cluster) for cluster in clusters)
-    cluster_table = []
-    for cluster, cluster_data in tqdm(enumerate(clusters), total=len(clusters)):
+    # Save clustering and images of cluster centers
+    print('Saving clusters')
+    for cluster in trange(num_clusters):
         # Make cluster directory
         cluster_dir = os.path.join(save_dir, f'cluster_{cluster}')
         makedirs(cluster_dir)
 
-        # Get smiles and zinc indices for molecules in cluster
-        smiles, zinc_indices, in_train, in_train_hit, pred = zip(*[
-            (
-                datapoint['smiles'],
-                datapoint['zinc_index'],
-                datapoint['in_train'],
-                datapoint['in_train_hit'],
-                datapoint['50uMInhibition_Avg']
-            )
-            for datapoint in cluster_data
-        ])
+        # Get cluster data and center
+        cluster_data = clusters[cluster]
+        cluster_center = kmeans.cluster_centers_[cluster]
 
-        # Add smiles and zinc indices to cluster table
-        blanks = [''] * (max_cluster_size - len(smiles))
-        cluster_table.append([f'cluster_{cluster}_smiles'] + list(smiles) + blanks)
-        cluster_table.append([f'cluster_{cluster}_zinc_indices'] + list(zinc_indices) + blanks)
-        cluster_table.append([f'cluster_{cluster}_in_train'] + list(in_train) + blanks)
-        cluster_table.append([f'cluster_{cluster}_in_train_hit'] + list(in_train_hit) + blanks)
-        cluster_table.append([f'cluster_{cluster}_50uMInhibition_Avg'] + list(pred) + blanks)
+        # Sort cluster data by prediction
+        if sort_column is not None:
+            cluster_data.sort(key=lambda datapoint: datapoint[sort_column], reverse=True)
 
-        # Add smiles and zinc indices to cluster table and save molecule images
-        for smile, zinc_index in zip(smiles, zinc_indices):
-            mol = Chem.MolFromSmiles(smile)
-            Draw.MolToFile(mol, os.path.join(cluster_dir, f'{zinc_index}.png'))
+        # Get top molecule
+        top_datapoint = cluster_data[0]
 
-    # Save cluster table
-    cluster_table = np.array(cluster_table).T
-    with open(os.path.join(save_dir, 'clusters.csv'), 'w') as f:
-        writer = csv.writer(f)
+        # Save image of top molecule
+        mol = Chem.MolFromSmiles(top_datapoint['smiles'])
+        Draw.MolToFile(mol, os.path.join(cluster_dir, f'center_{top_datapoint["zinc_index"]}.png'))
 
-        for row in cluster_table:
-            writer.writerow(row)
+        # Find molecule neareest to cluster center
+        center_datapoint, center_distance = None, float('inf')
+        for datapoint in cluster_data:
+            distance = np.linalg.norm(cluster_center - datapoint['morgan'])
+            if distance < center_distance:
+                center_distance = distance
+                center_datapoint = datapoint
+
+        # Save image of cluster center
+        mol = Chem.MolFromSmiles(center_datapoint['smiles'])
+        Draw.MolToFile(mol, os.path.join(cluster_dir, f'center_{center_datapoint["zinc_index"]}.png'))
+
+        # Pop morgan vector
+        for datapoint in cluster_data:
+            del datapoint['morgan']
+
+        # Save cluster data
+        with open(os.path.join(cluster_dir, f'cluster_{cluster}.csv'), 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(cluster_data)
 
 
 if __name__ == '__main__':
@@ -103,18 +91,15 @@ if __name__ == '__main__':
                         help='Path to CSV file containing SMILES strings and zinc indices')
     parser.add_argument('--save_dir', type=str, required=True,
                         help='Path to directory where the clustering and PNG images of molecules will be saved')
-    parser.add_argument('--train_smiles_path', type=str, required=True,
-                        help='Path to CSV file containing the training smiles')
-    parser.add_argument('--train_hits_smiles_path', type=str, required=True,
-                        help='Path to CSV file containing the training hit smiles')
-    parser.add_argument('--num_clusters', type=int, default=250,
+    parser.add_argument('--num_clusters', type=int, default=50,
                         help='Number of clusters')
+    parser.add_argument('--sort_column', type=str, default=None,
+                        help='Column name by which to sort the data')
     args = parser.parse_args()
 
     cluster_zinc_molecules(
         data_path=args.data_path,
         save_dir=args.save_dir,
-        train_smiles_path=args.train_smiles_path,
-        train_hits_smiles_path=args.train_hits_smiles_path,
-        num_clusters=args.num_clusters
+        num_clusters=args.num_clusters,
+        sort_column=args.sort_column
     )
