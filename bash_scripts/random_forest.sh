ca79305dbe68b619e46ad98f47133db0535c11de
Kyle Swanson <swansonk.14@gmail.com>
2019-07-07 14:57:50 -0400

    Updating scripts

--- bash_scripts/random_forest.sh
+++ bash_scripts/random_forest.sh
@@ -9,7 +9,7 @@ sizes=('one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' 'one' '
 # metrics=('rmse')
 # sizes=('one')
 
-split_type="scaffold"
+split_type="random"
 
 folds=(0 1 2 3 4 5 6 7 8 9)
 
