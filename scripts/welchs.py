f65a4bae1f3c41b61dc64fe1488b45e132750339
Kyle Swanson <swansonk.14@gmail.com>
2019-07-09 05:43:23 -0400

    Fixing welch's test

--- scripts/welchs.py
+++ scripts/welchs.py
@@ -20,9 +20,13 @@ def welchs(mean1: List[float],  # mean performance across folds for each dataset
 
     assert len(mean1) == len(std1) == len(nobs1) == len(mean2) == len(std2) == len(nobs2)
 
+    # Convert from population standard deviation to sample standard deviation
+    std1 = [s * np.sqrt(n / (n - 1)) for s, n in zip(std1, nobs1)]
+    std2 = [s * np.sqrt(n / (n - 1)) for s, n in zip(std2, nobs2)]
+
     # Compute Welch's t-test p-values for each dataset based on mean, standard deviation, and number of observations
     pvalues = [
-        stats.ttest_ind_from_stats(mean1=m1, std1=s1, nobs1=n1, mean2=m2, std2=s2, nobs2=n2, equal_var=False).pvalue
+        stats.ttest_ind_from_stats(mean1=m1, std1=s1, nobs1=n1, mean2=m2, std2=s2, nobs2=n2, equal_var=False).pvalue / 2
         for m1, s1, n1, m2, s2, n2 in zip(mean1, std1, nobs1, mean2, std2, nobs2)
     ]
 
@@ -31,15 +35,14 @@ def welchs(mean1: List[float],  # mean performance across folds for each dataset
 
     # Chi-squared statistic
     chisquare = -2 * np.sum(np.log(pvalues))
+    print(f'X^2  = {chisquare}')
 
     # Degrees of freedom
     df = 2 * len(pvalues)
+    print(f'df = {df}')
 
-    # Two-sided p-value for chi-squared (sf = survival function)
-    pvalue = stats.distributions.chi2.sf(chisquare, df=df)
-
-    # One-sided p-value for chi-squared
-    pvalue = pvalue / 2
+    # Two-sided p-value for chi-squared
+    pvalue = 1 - stats.distributions.chi2.cdf(chisquare, df=df)
 
     # Print p-value
     print(f'p = {pvalue:.4e}')
