db947578e2932151c9200674d366371633db5502
lhirschfeld <liortulip@gmail.com>
2019-07-27 12:25:40 -0400

    Updated table design.

--- web/frontend/src/components/pages/HomePage.tsx
+++ web/frontend/src/components/pages/HomePage.tsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
-import { Alert, Divider, Typography, Layout, Row, Col, Icon, Table } from 'antd';
+import {
+    Alert,
+    Divider,
+    Typography,
+    Layout,
+    Row,
+    Col,
+    Icon,
+    Table
+} from 'antd';
 
 import { page, pageName } from '../../api/constants';
 
@@ -15,36 +24,36 @@ export class HomePage extends Component<HomePageProps> {
     render() {
         const dataSource = [
             {
-              key: '1',
-              name: 'Mike',
-              age: 32,
-              address: '10 Downing Street',
+                key: '1',
+                name: 'Mike',
+                age: 32,
+                address: '10 Downing Street'
             },
             {
-              key: '2',
-              name: 'John',
-              age: 42,
-              address: '10 Downing Street',
-            },
-          ];
-          
-          const columns = [
+                key: '2',
+                name: 'John',
+                age: 42,
+                address: '10 Downing Street'
+            }
+        ];
+
+        const columns = [
             {
-              title: 'Name',
-              dataIndex: 'name',
-              key: 'name',
-            },
-            {
-              title: 'Age',
-              dataIndex: 'age',
-              key: 'age',
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name'
             },
             {
-              title: 'Address',
-              dataIndex: 'address',
-              key: 'address',
+                title: 'Age',
+                dataIndex: 'age',
+                key: 'age'
             },
-          ];
+            {
+                title: 'Address',
+                dataIndex: 'address',
+                key: 'address'
+            }
+        ];
         return (
             <Content
                 style={{
@@ -61,35 +70,37 @@ export class HomePage extends Component<HomePageProps> {
             >
                 <Row gutter={32}>
                     <Col className="gutter-row" span={8}>
-                        <div
-                            className="gutter-box"
-                            style={{ backgroundColor: '#00a0e9', padding: 10 }}
-                        >
-                            <h3 style={{ marginBottom: 0, color: 'white', fontWeight: 800 }}>
-                                DATA
-                            </h3>
-                        </div>
-                        <Table dataSource={dataSource} columns={columns} bordered size="middle" />
+                        <Table
+                            dataSource={dataSource}
+                            columns={columns}
+                            bordered
+                            size="small"
+                            title={() => (
+                                <div style={{ fontWeight: 800 }}>Data</div>
+                            )}
+                        />
                     </Col>
                     <Col className="gutter-row" span={8}>
-                        <div
-                            className="gutter-box"
-                            style={{ backgroundColor: '#00a0e9', padding: 10  }}
-                        >
-                            <h2 style={{ marginBottom: 0, color: 'white' }}>
-                                Checkpoints
-                            </h2>
-                        </div>
-                    </Col>{' '}
+                        <Table
+                            dataSource={dataSource}
+                            columns={columns}
+                            bordered
+                            size="small"
+                            title={() => (
+                                <div style={{ fontWeight: 800 }}>Checkpoints</div>
+                            )}
+                        />
+                    </Col>
                     <Col className="gutter-row" span={8}>
-                        <div
-                            className="gutter-box"
-                            style={{ backgroundColor: '#00a0e9', padding: 10  }}
-                        >
-                            <h2 style={{ marginBottom: 0, color: 'white' }}>
-                                Predictions
-                            </h2>
-                        </div>
+                        <Table
+                            dataSource={dataSource}
+                            columns={columns}
+                            bordered
+                            size="small"
+                            title={() => (
+                                <div style={{ fontWeight: 800 }}>Predictions</div>
+                            )}
+                        />
                     </Col>
                 </Row>
             </Content>
