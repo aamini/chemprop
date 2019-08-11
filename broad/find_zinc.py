91750d4781c8be6cc31e6d1ba67e5e4aa1d5e6b5
Kyle Swanson <kswanson@asapp.com>
2019-08-10 23:02:11 -0400

    Multiprocess

--- broad/find_zinc.py
+++ broad/find_zinc.py
@@ -1,10 +1,36 @@
 from argparse import ArgumentParser
 import csv
+from functools import partial
 import json
+from multiprocessing import Pool
 import os
 from tqdm import tqdm
+from typing import Dict
 
 from standardize_smiles import standardize_smiles
+
+
+def find_in_tranche(path: str, missing: Dict[str, dict], standard_to_original: Dict[str, str]):
+    tranche = os.path.splitext(os.path.basename(path))[0]
+    print(tranche)
+
+    with open(path) as f:
+        for row in tqdm(csv.DictReader(f, delimiter='\t')):
+            smiles = row['smiles']
+
+            zinc_row = {
+                'smiles': smiles,
+                'zinc_index': row['zinc_id'],
+                'tranche': tranche
+            }
+
+            if smiles in missing:
+                missing['smiles']['normal'].append(zinc_row)
+
+            standard_smiles = standardize_smiles(smiles)
+
+            if standard_smiles in standard_to_original:
+                missing[standard_to_original[standard_smiles]]['standard'].append(zinc_row)
 
 
 def find_zinc(missing_path: str, zinc_dir: str, save_path: str):
@@ -28,28 +54,11 @@ def find_zinc(missing_path: str, zinc_dir: str, save_path: str):
     print(f'{len(missing)} missing zinc indices')
 
     paths = sorted([os.path.join(zinc_dir, fname) for fname in os.listdir(zinc_dir) if fname.endswith('.txt')])
+    
+    find = partial(find_in_tranche, missing=missing, standard_to_original=standard_to_original)
 
-    for path in paths:
-        tranche = os.path.splitext(os.path.basename(path))[0]
-        print(tranche)
-
-        with open(path) as f:
-            for row in tqdm(csv.DictReader(f, delimiter='\t')):
-                smiles = row['smiles']
-
-                zinc_row = {
-                    'smiles': smiles,
-                    'zinc_index': row['zinc_id'],
-                    'tranche': tranche
-                }
-
-                if smiles in missing:
-                    missing['smiles']['normal'].append(zinc_row)
-
-                standard_smiles = standardize_smiles(smiles)
-
-                if standard_smiles in standard_to_original:
-                    missing[standard_to_original[standard_smiles]]['standard'].append(zinc_row)
+    with Pool() as pool:
+        pool.map(find, paths)
 
     with open(save_path, 'w') as f:
         json.dump(missing, f, indent=4, sort_keys=True)
